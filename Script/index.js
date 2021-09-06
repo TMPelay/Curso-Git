ff95947d587e26a79f7d8d8d4cbc3d325a9db104
TMPelay <tomasmalleret@gmail.com>
2021-09-06 08:15:55 -0300

    FUnciones agregadas

--- Script/index.js
+++ Script/index.js
@@ -1,5 +1,19 @@
+
+let suma = Sumar(2, 3);
+let resta = Restar(10, 5);
+
 function Saludar() {
   console.log("hola mundo");
 }
 
+function Sumar(num1, num2) {
+  return num1 + num2;
+}
+
+function Restar(num1, num2) {
+  return num1 - num2;
+}
+
 Saludar();
+console.log(suma);
+console.log(resta);
